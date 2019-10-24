2b9e20cb2d40309269bb5efc8107789b928155a1
Jake Dingley <jdingley@hotmail.com>
2019-10-23 23:43:47 -0400

    updated services, added AOF

--- controllers/ServicesController.js
+++ controllers/ServicesController.js
@@ -5,6 +5,23 @@ exports.RenderServicesPage = function(req, res, next) {
     res.render('Services/payment_page', { title: "Payments" });
   }
   exports.RenderIntakePage = function(req, res, next) {
-    res.render('Services/services_page', { title: "Intake Forms and Consent" });
+    res.render('Services/intake_page', { title: "Intake Forms and Consent" });
   }
-  
+  exports.RenderAreaOfFocusPage = function(req, res, next) {
+    res.render('Services/AOF/areas_of_focus', { title: "Areas of Focus" });
+  }
+  exports.RenderPreteens = function(req, res, next) {
+    res.render('Services/AOF/preteen', { title: "Areas of Focus" });
+  }
+  exports.RenderTeens = function(req, res, next) {
+    res.render('Services/AOF/teen', { title: "Areas of Focus" });
+  }
+  exports.RenderAdults = function(req, res, next) {
+    res.render('Services/AOF/adult', { title: "Areas of Focus" });
+  }
+  exports.RenderElderly= function(req, res, next) {
+    res.render('Services/AOF/elderly', { title: "Areas of Focus" });
+  }
+  exports.RenderDisabilities= function(req, res, next) {
+    res.render('Services/AOF/disabilites', { title: "Areas of Focus" });
+  }
--- routes/services.js
+++ routes/services.js
@@ -6,5 +6,11 @@ const ServicesController = require('../controllers/ServicesController');
 router.get('/Services', ServicesController.RenderServicesPage);
 router.get('/MakePayements', ServicesController.RenderPaymentsPage);
 router.get('/ConsentForms', ServicesController.RenderIntakePage);
+router.get('/Services/AOF', ServicesController.RenderAreaOfFocusPage);
+router.get('/Services/AOF/Preteens', ServicesController.RenderPreteens);
+router.get('/Services/AOF/Teens', ServicesController.RenderTeens);
+router.get('/Services/AOF/Adults', ServicesController.RenderAdults);
+router.get('/Services/AOF/Elderly', ServicesController.RenderElderly);
+router.get('/Services/AOF/Disabilities', ServicesController.RenderDisabilities);
 
 module.exports = router;
