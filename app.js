1108a00666713239e4ee6b0e5e56198dce13ba2c
Jake Dingley <jdingley@hotmail.com>
2020-01-31 17:09:57 -0500

    Finished Scheduler

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var http = require('http');
-var app = express();
-var server = http.createServer(app);
-const mysql = require('mysql');
 var session = require("express-session");
-var bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+var server = http.createServer(app);
+// use body parse for parsing POST request
+const bodyParser = require("body-parser");
+const port = 3000;
+
+// we'll use mysql for db access and util to promisify queries
 const util = require("util");
+const mysql = require('mysql');
+
+// use your own parameters for database
+const mysqlConfig = {
+	"connectionLimit": 10,
+	"host": "localhost",
+	"user": "root",
+	"password": "",
+  "database": "WebsiteDB",
+};
+
 const helmet = require("helmet");
 app.use(helmet());
-
-server.listen(3000);
-
-var db = mysql.createPool ({
-  "connectionLimit": 10,
-  "host": "localhost",
-  "user": "root",
-  "password": "",
-  "database": "WebsiteDB"
-});
 
 var homeRouter = require('./routes/home');
 var aboutRouter = require('./routes/about');
@@ -35,8 +39,6 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');    
 app.set('views', __dirname +'/views'); 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,6 +49,38 @@ app.use('/', aboutRouter);
 app.use('/', servicesRouter);
 app.use('/', contactRouter);
 app.use('/', blogRouter);
+
+// scheduler sends application/x-www-form-urlencoded requests,
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// you'll need these headers if your API is deployed on a different domain than a public page
+// in production system you could set Access-Control-Allow-Origin to your domains
+// or drop this expression - by default CORS security is turned on in browsers
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "*");
+	next();
+});
+
+// return static pages from "./public" directory
+app.use(express.static(__dirname + "/public"));
+
+const router = require("./router");
+
+// open connection to mysql
+const connectionPool = mysql.createPool(mysqlConfig);
+connectionPool.query = util.promisify(connectionPool.query);
+
+// add listeners to basic CRUD requests
+const Storage = require("./storage");
+const eventsStorage = new Storage(connectionPool);
+router.setRoutes(app, "/events", eventsStorage);
+
+// start server
+app.listen(port, () => {
+	console.log("Server is running on port " + port + "...");
+});
 
 app.use(function(req, res, next) {
   res.status(400).render('Error/denied_access', {
@@ -75,24 +109,6 @@ app.use(function(err, req, res, next) {
   });  
 }); 
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Methods", "*");
-	next();
-});
-app.use(express.static(__dirname + "/public"));
-
-const router = require("./router");
-
-// open connection to mysql
-const connectionPool = mysql.createPool(db);
-connectionPool.query = util.promisify(connectionPool.query);
-
-// add listeners to basic CRUD requests
-const Storage = require("./storage");
-const eventsStorage = new Storage(connectionPool);
-router.setRoutes(app, "/events", eventsStorage);
-
-
 module.exports = app;
+
+
