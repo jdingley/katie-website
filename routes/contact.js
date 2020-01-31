50f7e2c559fa79caefb8b79981aee80539c58cac
Jake Dingley <jdingley@hotmail.com>
2020-01-31 17:43:13 -0500

    fixed routes

--- routes/contact.js
+++ routes/contact.js
@@ -5,6 +5,5 @@ const ContactController = require('../controllers/ContactController');
 
 router.get('/Contact', ContactController.RenderContactPage);
 router.get('/Schedule', ContactController.RenderSchedule);
-router.post('/Schedule/post', ContactController.ScheduleAppointment);
 router.get('/AdminLogin', ContactController.RenderAdminLogin);
 module.exports = router;
